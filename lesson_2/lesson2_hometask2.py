93483206cd1a40bbffec9982b388508d5ec79681
Andrii <abasara2003@gmail.com>
2023-01-04 14:16:37 +0200

    added lesson 2 second homework

--- lesson_2/lesson2_hometask2.py
+++ lesson_2/lesson2_hometask2.py
@@ -1,10 +1,16 @@
-## Stage 0 - setting pairs
+def is_prime(a):
+    if a % 2 == 0:
+        return a == 2
+    d = 3
+    while d * d <= a and a % d != 0:
+        d += 2
+    return d * d > a
 def create_pairs():
     arr_2d = []
 
     i = 2
     j = 2
-    for row in range(9_604):
+    for row in range(9604):
         internal_arr = []
 
         for col in range(2):
@@ -16,119 +22,102 @@ def create_pairs():
 
         arr_2d.append(internal_arr)
     
-        if j == 100:
+        if j == 99:
             i += 1
             j = 2
 
     return arr_2d
-        
-pairs = create_pairs()
-
-## Stage 1 - Prod: I don't know the numbers
-def is_prime(a):
-    if a % 2 == 0:
-        return a == 2
-    d = 3
-    while d * d <= a and a % d != 0:
-        d += 2
-    return d * d > a
-
-check = False
-count = 0
-while count in range(len(pairs)):
-    internal_arr = pairs[count]
-
-    if is_prime(internal_arr[0]) == True and is_prime(internal_arr[1]) == True:
-        check = True    
-
-    elif is_prime(internal_arr[0]) == True and internal_arr[0] > 50 or is_prime(internal_arr[1]) == True and internal_arr[1] > 50:
-        check = True 
-
-    if check == True:
-        pairs.pop(count)
-        count -= 1
-
+def stage_1(pairs):
     check = False
-    count += 1
-
-## Stage 2 - Sum: I know it
-check = False
-count = 0
-while count in range(len(pairs)):
-    internal_arr = pairs[count]
-
-    if (internal_arr[0] + internal_arr[1]) % 2 == 0:
-        check = True
-
-    elif internal_arr[0] + internal_arr[1] > 52:
-        check = True
-
-    elif is_prime((internal_arr[0] + internal_arr[1]) - 2) == True:
-        check = True
-
-    if check == True:
-        pairs.pop(count)
-        count -= 1
-
-    check = False
-    count += 1
-
-## Stage 3 - Prod: then I know the numbers;
-##           Sum: then I know the numbers too.
-def setSum(sum):
-    count = 0    
+    count = 0
     while count in range(len(pairs)):
-        internal_arr = pairs[count]  
+        internal_arr = pairs[count]
 
-        if internal_arr[0] + internal_arr[1] > sum:
-            sum = internal_arr[0] + internal_arr[1]
-            break
+        if is_prime(internal_arr[0])and is_prime(internal_arr[1]):
+           check = True    
 
+        if check:
+           pairs.pop(count)
+           count -= 1
+
+        check = False
         count += 1
-
-    return sum
-
-check = False
-checkPlus = False
-count = 0
-checkCount = 0
-Sum = 0
-
-while check == False:
-    sum = setSum(Sum)
-    Sum = sum    
-    if sum > 50:
-        checkPlus = True
-
+def stage_2(pairs):
+    check = False
+    count = 0
     while count in range(len(pairs)):
-        internal_arr = pairs[count]    
+        internal_arr = pairs[count]
 
-        if internal_arr[0] + internal_arr[1] == sum:
-            if internal_arr[0] == pow(2, 2) and is_prime(internal_arr[1]) == True or internal_arr[0] == pow(2, 3) and is_prime(internal_arr[1]) == True or internal_arr[0] == pow(2, 4) and is_prime(internal_arr[1]) == True or internal_arr[0] == pow(2, 5) and is_prime(internal_arr[1]) == True:
-                checkCount += 1                
+        if (internal_arr[0] + internal_arr[1]) % 2 == 0:
+            check = True
 
-            elif internal_arr[1] == pow(2, 2) and is_prime(internal_arr[0]) == True or internal_arr[1] == pow(2, 3) and is_prime(internal_arr[0]) == True or internal_arr[1] == pow(2, 4) and is_prime(internal_arr[0]) == True or internal_arr[1] == pow(2, 5) and is_prime(internal_arr[0]) == True:
-                checkCount += 1                
+        elif internal_arr[0] + internal_arr[1] > 52:
+            check = True
+
+        elif is_prime((internal_arr[0] + internal_arr[1]) - 2) == True:
+            check = True
+
+        if check == True:
+            pairs.pop(count)
+            count -= 1
+
+        check = False
+        count += 1
+def stage_3(pairs):
+   def setSum(sum):
+       count = 0    
+       while count in range(len(pairs)):
+           internal_arr = pairs[count]  
+
+           if internal_arr[0] + internal_arr[1] > sum:
+               sum = internal_arr[0] + internal_arr[1]
+               break
+
+           count += 1
+
+       return sum
+
+   check = False
+   checkPlus = False
+   count = 0
+   checkCount = 0
+   Sum = 0
+
+   while check == False:
+       sum = setSum(Sum)
+       Sum = sum    
+       if sum > 50:
+           checkPlus = True
+
+       while count in range(len(pairs)):
+           internal_arr = pairs[count]    
+
+           if internal_arr[0] + internal_arr[1] == sum:
+               if internal_arr[0] == pow(2, 2) and is_prime(internal_arr[1]) == True or internal_arr[0] == pow(2, 3) and is_prime(internal_arr[1]) == True or internal_arr[0] == pow(2, 4) and is_prime(internal_arr[1]) == True or internal_arr[0] == pow(2, 5) and is_prime(internal_arr[1]) == True:
+                   checkCount += 1                
+
+               elif internal_arr[1] == pow(2, 2) and is_prime(internal_arr[0]) == True or internal_arr[1] == pow(2, 3) and is_prime(internal_arr[0]) == True or internal_arr[1] == pow(2, 4) and is_prime(internal_arr[0]) == True or internal_arr[1] == pow(2, 5) and is_prime(internal_arr[0]) == True:
+                   checkCount += 1                
   
-        count += 1
+           count += 1
 
-    if checkCount > 2:
-        checkCount = 0
-        count = 0
-        while count in range(len(pairs)):
-            internal_arr = pairs[count]
+       if checkCount > 2:
+           checkCount = 0
+           count = 0
+           while count in range(len(pairs)):
+               internal_arr = pairs[count]
 
-            if internal_arr[0] + internal_arr[1] == sum:
-                pairs.pop(count)
-                count -= 1
+               if internal_arr[0] + internal_arr[1] == sum:
+                   pairs.pop(count)
+                   count -= 1
 
-            count += 1
-        count = 0
-    else:
-        checkCount = 0
-        count = 0
-        if checkPlus == True:
-            check = True    
+               count += 1
+           count = 0
+       else:
+           checkCount = 0
+           count = 0
+           if checkPlus == True:
+               check = True    
 
 ##
 # 29 = 25 + 4
@@ -137,14 +126,14 @@ while check == False:
 # 16 * 25 = 400 = 80 * 5 -> X
 ##
 
-while count in range(len(pairs)):
-    internal_arr = pairs[count]
+   while count in range(len(pairs)):
+       internal_arr = pairs[count]
 
-    if internal_arr[0] + internal_arr[1] == 29 or internal_arr[0] + internal_arr[1] == 41:
-        pairs.pop(count)
-        count -= 1
+       if internal_arr[0] + internal_arr[1] == 29 or internal_arr[0] + internal_arr[1] == 41:
+           pairs.pop(count)
+           count -= 1
 
-    count += 1
+       count += 1
 
 ##
 # 17 = 2 + 15, 2 * 15 = 5 * 6 (sum is 11) — bad prod
@@ -156,16 +145,28 @@ while count in range(len(pairs)):
 # 17 = 8 + 9, 8 * 9 = 24 * 3 (sum is 27) — bad prod
 ##
 
-count = 0
-while count in range(len(pairs)):
-    internal_arr = pairs[count]
+   count = 0
+   while count in range(len(pairs)):
+       internal_arr = pairs[count]
 
-    if internal_arr[0] == 4 or internal_arr[0] == 13:
-        check = True
-    else:
-        pairs.pop(count)
-        count -= 1
+       if internal_arr[0] == 4 or internal_arr[0] == 13:
+           check = True
+       else:
+           pairs.pop(count)
+           count -= 1
 
-    count += 1
+       count += 1
 
-print('The result of the program is ' + str(pairs))
+## Stage 1 - Prod: I don't know the numbers
+## Stage 2 - Sum: I know it
+## Stage 3 - Prod: then I know the numbers;
+##           Sum: then I know the numbers too.
+
+def main():
+    pairs = create_pairs()
+    stage_1(pairs)
+    stage_2(pairs)
+    stage_3(pairs)
+    print('The result is ' + str(pairs))
+    
+main()
