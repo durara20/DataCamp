cf646ff4e6604e7700757fd8655c2691a647ddd4
Andrii <abasara2003@gmail.com>
2023-01-10 18:37:01 +0200

    5,7,8

--- lesson_5/5_1.py
+++ lesson_5/5_1.py
@@ -17,18 +17,15 @@ df_iris['label_names'] = df_iris.label.apply(lambda x: features_dict[x])
 X=iris.data
 y=iris.target
 
-X_train, X_test, y_train, y_test=train_test_split(X,y,test_size=0.2,random_state=1)
+X_train, X_test, y_train, y_test=train_test_split(X,y,test_size=0.2)
 
-scaler= MinMaxScaler()
-X_train_scaled= scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
  
 k_best = 0 # 'compute the best k'
 score_best = 0 #'compute the best score'
 for k in range(1,len(X_test)):#second parameter is a size of a test set
     model=KNN(k_neighbours=k)
-    model.fit(X_train_scaled,y_train)
-    score = model.score(X_test_scaled,y_test)
+    model.fit(X_train,y_train)
+    score = model.score(X_test,y_test)
     if score>score_best:
         score_best=score
         k_best=k
